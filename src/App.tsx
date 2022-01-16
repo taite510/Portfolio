7443e9c79797f728a59496776a8c440f2e8ac6b2
Taite Jernigan <taite5100@gmail.com>
2022-01-16 13:33:58 -0800

    Added buttons and changed their styling a bit

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,20 @@ import useConfig from './components/useConfig';
 import logo from './logo.svg';
 import Home from './home/Home';
 import CssBaseline from '@mui/material/CssBaseline';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 /**
  * Our Web Application
  */
+
+const buttonNames = ['Home', 'Resume', 'Extra']
+const buttons = buttonNames.map((b) => {
+  return <Button key={b} sx={{ bgcolor: '#617ec3', color: '#ddefff', height: '50px', fontSize: '18px', letterSpacing: '1px'}}>{b}</Button>
+});
+
 export default function App() {
   const config = useConfig();
   return (
@@ -17,11 +28,23 @@ export default function App() {
       {/* <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <h1 className='App-title'>Welcome to {config.app.TITLE}</h1>
-      </header>
-      <p className='App-intro'>
+        </header>
+        <p className='App-intro'>
         To get started, edit <code>src/App.tsx</code> and save to reload.
       </p> */}
-      <Home></Home>
+      <Container maxWidth='sm' sx={{ position: 'relative' }}>
+        <ButtonGroup
+          orientation='vertical'
+          aria-label='vertical outlined button group'
+          variant='contained'
+          sx={{ color: '#495a81', position: 'absolute', left: '-120px', width: '130px', height: 'auto' }}
+        >
+          {buttons}
+        </ButtonGroup>
+        <Box sx={{ bgcolor: '#495a81', height: 'auto', margin: '80px 0', borderRadius: '10px', padding: '10px' }}>
+          <Home></Home>
+        </Box>
+      </Container>
     </div>
   );
 }
--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import Button from '@mui/material/Button';
 import { jsx, css } from '@emotion/react'
 import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import myAvatar from '../../public/photos/taite.jpg'
 import Typography from '@mui/material/Typography';
@@ -16,32 +15,28 @@ import Link from '@mui/material/Link';
 export default function Home() {
   return (
     <React.Fragment>
-      <Container maxWidth='sm'>
-        <Box sx={{ bgcolor: '#495a81', height: 'auto', margin: '80px 0', borderRadius: '10px', padding: '10px' }}>
-          <Avatar alt='Taite Jernigan' src={myAvatar}  sx={{ width: 250, height: 250, margin: 'auto'}}/>
-          <Typography variant="h3" component="div" sx={{color: '#cfe6fa'}}>
-          Hi, I'm <b style={{color: '#09203d'}}>Taite Jernigan</b>
-          </Typography>
-          <Typography variant="h5" component="div" gutterBottom sx={{color: '#cfe6fa', fontStyle: 'italic'}}>
-          web designer & developer
-          </Typography>
-          <Divider variant="middle" sx={{borderBottomWidth: '1.5px', borderColor: '#434363', margin: '30px 0'}}/>
-          <Typography variant="body1" component="div" gutterBottom sx={{color: '#cfe6fa'}}>
-          Former marine biologist / Bering Sea survivor turned webdev. <br/> Enjoys building React apps and designing backend archetecture. <br/> Please hire him.
-          </Typography>
-          <Box sx={{ bgcolor: '#546da6', height: '50px', borderRadius: '10px', padding: '10px', margin: '20px 0' }}>
-          <Typography variant='subtitle1' component='div' sx={{color: '#cfe6fa', fontSize: '1.05rem'}}>
-            email: taite5100@gmail.com &nbsp;&nbsp;&nbsp;<b>/</b>&nbsp;&nbsp;&nbsp; phone: (562) 852-2919
-          </Typography>
-        </Box>
-        <Link href='https://github.com/taite510' target="_blank" sx={{color: '#09203d', margin: '0 20px'}}>
-          <GitHubIcon fontSize="large"/>
-        </Link>
-        <Link href='https://www.linkedin.com/in/taite-jernigan/' target="_blank" sx={{color: '#09203d', margin: '0 20px'}}>
-          <LinkedInIcon fontSize="large"/>
-        </Link>
-        </Box>
-      </Container>
+      <Avatar alt='Taite Jernigan' src={myAvatar}  sx={{ width: 250, height: 250, margin: 'auto'}}/>
+      <Typography variant='h3' component='div' sx={{color: '#cfe6fa'}}>
+        Hi, I'm <b style={{color: '#09203d'}}>Taite Jernigan</b>
+      </Typography>
+      <Typography variant='h5' component='div' gutterBottom sx={{color: '#cfe6fa', fontStyle: 'italic'}}>
+        web designer & developer
+      </Typography>
+      <Divider variant='middle' sx={{borderBottomWidth: '1.5px', borderColor: '#434363', margin: '30px 0'}}/>
+      <Typography variant='body1' component='div' gutterBottom sx={{color: '#cfe6fa'}}>
+        Former marine biologist / Bering Sea survivor turned webdev. <br/> Enjoys building React apps and designing backend architecture. <br/> Please hire him.
+      </Typography>
+      <Box sx={{ bgcolor: '#546da6', height: 'auto', borderRadius: '5px', padding: '10px', margin: '20px 0' }}>
+        <Typography variant='subtitle1' component='div' sx={{color: '#cfe6fa', fontSize: '1.05rem'}}>
+          email: taite5100@gmail.com &nbsp;&nbsp;&nbsp;<b>/</b>&nbsp;&nbsp;&nbsp; phone: (562) 852-2919
+        </Typography>
+      </Box>
+      <Link href='https://github.com/taite510' target='_blank' sx={{color: '#09203d', margin: '0 20px'}}>
+        <GitHubIcon fontSize='large'/>
+      </Link>
+      <Link href='https://www.linkedin.com/in/taite-jernigan/' target='_blank' sx={{color: '#09203d', margin: '0 20px'}}>
+        <LinkedInIcon fontSize='large'/>
+      </Link>
     </React.Fragment>
   )
 }
