efc8d9c9276185a8e8a7d2f0c02f4112a3b89669
Taite Jernigan <taite5100@gmail.com>
2022-01-15 19:00:03 -0800

    Added MUI to dependencies, created a home page with a basic outline

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import "./App.css";
+import './App.css';
 
-import * as React from "react";
+import * as React from 'react';
 
-import useConfig from "./components/useConfig";
-import logo from "./logo.svg";
-
+import useConfig from './components/useConfig';
+import logo from './logo.svg';
+import Home from './home/Home';
+import CssBaseline from '@mui/material/CssBaseline';
 /**
  * Our Web Application
  */
 export default function App() {
   const config = useConfig();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to {config.app.TITLE}</h1>
+    <div className='App'>
+      <CssBaseline />
+      {/* <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h1 className='App-title'>Welcome to {config.app.TITLE}</h1>
       </header>
-      <p className="App-intro">
+      <p className='App-intro'>
         To get started, edit <code>src/App.tsx</code> and save to reload.
-      </p>
+      </p> */}
+      <Home></Home>
     </div>
   );
 }
--- src/server/html.tsx
+++ src/server/html.tsx
@@ -30,6 +30,14 @@ const html = ({
       <script id="config-server-side">
         window.__CONFIG__ = ${JSON.stringify(config)};
       </script>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+      <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      />
     </head>
     <body>
       <div id="root">${content}</div>
