b62c3aca06bfd9e0eb28791981c7c775af0a7397
Taite Jernigan <taite5100@gmail.com>
2022-02-15 14:34:12 -0800

    Added some text to the projects section

--- src/modules/Projects.tsx
+++ src/modules/Projects.tsx
@@ -9,6 +9,8 @@ import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import EditIcon from '@mui/icons-material/Edit';
 import pictures from '../../public/data/photoData';
 
 let projectPics = pictures.projects
@@ -29,6 +31,10 @@ export default function About(props: any) {
       <Paper elevation={8} sx={{ bgcolor: '#495a81', height: '723px', margin: props.styles.margin, borderRadius: '10px', padding: '30px', width: '560px', overflow: 'auto'}}>
         <Typography variant='h4' component='div' gutterBottom sx={{ color: '#cfe6fa', textAlign: 'left' }}>Projects</Typography>
         <Divider variant='middle' sx={{borderBottomWidth: '1.5px', borderColor: '#0a1f3d', margin: '10px 0 10px 0'}}/>
+        <Typography variant='h5' component='div' sx={{ color: '#86c7ff', textAlign: 'left', margin: '20px 0 5px 0' }}>
+          <ShoppingBagIcon fontSize='large' sx={{ verticalAlign: 'bottom'}}/>
+          &nbsp; Project Catwalk
+        </Typography>
         <ImageList
           gap={4}
           cols={3}
@@ -59,13 +65,17 @@ export default function About(props: any) {
             }
           })}
         </ImageList>
+        <Typography variant='h5' component='div' sx={{ color: '#86c7ff', textAlign: 'left', margin: '20px 0 5px 0' }}>
+          <EditIcon fontSize='large' sx={{ verticalAlign: 'bottom'}}/>
+          &nbsp; study.io
+        </Typography>
         <ConstructionIcon sx={{fontSize: 60, margin: 'auto', color: '#86c7ff'}}/>
         <Typography variant='h6' sx={{color: '#86c7ff'}}>(under construction)</Typography>
       </Paper>
       <Modal open={open.isOpen} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
           <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 'auto', height: 'auto' }}>
             <ImageListItem>
-                  <img src={(projectPics as any)[open.img].link}/>
+                  <img src={(projectPics as any)[open.img].link} alt={(projectPics as any)[open.img].title}/>
                 </ImageListItem>
           </Box>
       </Modal>
