15642d791e7bbf9ade620c5844382cde0facd0ff
Taite Jernigan <taite5100@gmail.com>
2022-02-28 17:09:40 -0800

    changed resume link

--- src/modules/Resume.tsx
+++ src/modules/Resume.tsx
@@ -41,7 +41,7 @@ export default function Resume(props: any) {
         <Paper elevation={8} sx={{ bgcolor: '#495a81', height: styles.height, margin: styles.margin, borderRadius: '10px', padding: styles.padding, width: styles.width, overflow: styles.overflow}}>
           <Typography variant='h4' component='div' gutterBottom sx={{ color: '#cfe6fa', textAlign: 'left' }}>Resume</Typography>
           <Divider variant='middle' sx={{borderBottomWidth: '1.5px', borderColor: '#0a1f3d', margin: '10px 0 10px 0'}}/>
-          <Link href='https://drive.google.com/uc?export=download&id=1cC2EDOjec1jofFEgM5wQDfEVSIUb9THw' download='Taite Jernigan Resume.pdf' underline='none'>
+          <Link href='https://drive.google.com/uc?export=download&id=12TmeZKNsh9t3EZ8HbeStvDvgGoXbe1X_' download='Taite Jernigan Resume.pdf' underline='none'>
             <Button  sx={{ color: '#ddefff', marginTop: '5px'}}>
               <FileDownloadIcon/>
               Download My Resume
